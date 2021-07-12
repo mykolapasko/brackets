44b6ce2f5c8ab3fbb1b2dad65fa7a1aeb6168178
mykolapasko <nikpasko@gmail.com>
2021-07-12 13:33:40 +0300

    feat: add check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const singleTagBrackets = []
+  const doubleTagBrackets = []
+
+  bracketsConfig.forEach(item => {
+    item[0] === item[1] ? 
+    singleTagBrackets.push(item.join('')) : 
+    doubleTagBrackets.push(item.join(''))
+  });
+
+  const bracketsReducer = function(string) {
+    let startLen = string.length
+    doubleTagBrackets.forEach(item => {
+      string = string.replace(item, '')
+    })
+    singleTagBrackets.forEach(item => {
+      string = string.replace(item, '')
+    })
+    let endLen = string.length
+    if (startLen !== endLen) {
+      return bracketsReducer(string)
+    } else if (endLen === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+  return bracketsReducer(str)
 }
+
